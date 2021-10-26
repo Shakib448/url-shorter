923c268f9030977531c5ac0f2fc54cec3583acdc
Shakiba448 <shakiba448@gmail.com>
2021-10-26 19:28:20 +0600

    Success

--- client/pages/api/client.js
+++ client/pages/api/client.js
@@ -1,5 +1,5 @@
 import { create } from "apisauce";
 
-const apiClient = create({ baseURL: "http://localhost:5000/" });
+const apiClient = create({ baseURL: process.env.NEXT_HEROKU_URL });
 
 export default apiClient;
