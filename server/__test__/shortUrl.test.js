8205ac30e4a7403bc1c8ca40c354017d3f30aac2
Shakiba448 <shakiba448@gmail.com>
2021-10-27 18:49:13 +0600

    WIP on test

---
+++ server/__test__/shortUrl.test.js
@@ -0,0 +1,9 @@
+import supertest from "supertest";
+import express from "express";
+
+const app = express();
+describe("Test the root path", () => {
+  test("It should response the GET method", () => {
+    console.log("My first test");
+  });
+});
