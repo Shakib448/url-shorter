9cce8980f5ae199c5543f685f19ab85e72e9af08
Shakiba448 <shakiba448@gmail.com>
2021-10-28 06:49:40 +0600

    WIP on server testing

--- server/__test__/shortUrl.test.js
+++ server/__test__/shortUrl.test.js
@@ -1,5 +1,8 @@
 import supertest from "supertest";
 import app from "../app";
+import mongoose from "mongoose";
+import { MongoMemoryServer } from "mongodb-memory-server";
+
 describe("Short url testing", () => {
   it("It should response the GET method", async () => {
     const res = await supertest(app).get("/api/shortUrl");
