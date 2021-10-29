7489f3d08a5dcdd3f210a5778aeaa4d75a8e3d45
Shakiba448 <shakiba448@gmail.com>
2021-10-29 11:50:12 +0600

    WIP on testing

--- server/__test__/shortUrl.test.js
+++ server/__test__/shortUrl.test.js
@@ -1,11 +1,11 @@
 import supertest from "supertest";
 import app from "../app";
-import mongoose from "mongoose";
-import { MongoMemoryServer } from "mongodb-memory-server";
 
 describe("Short url testing", () => {
   it("It should response the GET method", async () => {
     const res = await supertest(app).get("/api/shortUrl");
     expect(res.status).toEqual(200);
+    expect(Array.isArray(res.body)).toBeTruthy();
+    expect(res.body.length).toEqual(1);
   });
 });
