59b8030432ea780a6e494aff0bbb4dd4f96996b1
Shakiba448 <shakiba448@gmail.com>
2021-10-30 14:40:12 +0600

    WIP on testing

--- server/__test__/shortUrl.test.js
+++ server/__test__/shortUrl.test.js
@@ -1,6 +1,9 @@
 import supertest from "supertest";
 import app from "../app";
+import time from "../utils/time";
+import sinon from "sinon";
 
+sinon.stub(time, "setTimeout");
 describe("Short url testing", () => {
   it("It should response the GET method", async () => {
     const res = await supertest(app).get("/api/shortUrl");
