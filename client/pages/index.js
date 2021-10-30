2c7b9d32280ea0b07c5322c5a166063822721c20
Shakiba448 <shakiba448@gmail.com>
2021-10-30 15:33:51 +0600

    WIP on testing

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { Container } from "@mui/material";
 
-import { Layout, UrlShrinkForm, UrlShrinkTable } from "../src/Components";
+import { Layout, UrlShrinkForm, UrlShrinkTable } from "@/components/index";
+
 import urlApi from "./api/URL";
 
 const Home = () => {
